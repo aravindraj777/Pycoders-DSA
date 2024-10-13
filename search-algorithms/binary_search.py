1699a5b42ef7c66c417bb12678c3122431038193
Aravind <aravindmunna240@gmail.com>
2024-10-13 15:17:53 +0530

    binary search

---
+++ search-algorithms/binary_search.py
@@ -0,0 +1,27 @@
+def binary_search(arr, value):
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] < value:
+            left = mid + 1
+        elif arr[mid] > value:
+            right = mid - 1
+        else:
+            return mid
+    return -1
+
+
+
+
+arr = [1,2,3,4,5]
+value = 2
+result = binary_search(arr, value)
+print(result)
+
+if(result != -1):
+    print(f"value present in the index {result}")
+else:
+    print("Value not present in the list ")
