2aebcc5f2061f14ccb33278d077034765302329d
Aravind <aravindmunna240@gmail.com>
2025-02-17 23:22:03 +0530

    DSA: Rotated sort array

---
+++ leetcodeDSA/Rotated_Sort_Array.py
@@ -0,0 +1,15 @@
+def find_min(nums):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return nums[left]
+
+# Example Usage
+nums = [4,5,6,7,0,1,2]
+print(find_min(nums))  # Output: 0
